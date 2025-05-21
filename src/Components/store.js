576bf1e041f0e4f05c6ca0d4f28029961403a6cf
Tewos-21 <tewodrosshimels54@gmail.com>
2025-05-21 00:17:49 -0700

    update all chahnges

--- src/Components/store.js
+++ src/Components/store.js
@@ -3,7 +3,9 @@ import cartReducer from './Components/CartSlice';
 // This file sets up the Redux store for the application
 // It imports the cart reducer and configures the store with it
 const store = configureStore({
+  // The configureStore function from Redux Toolkit is used to create the store
   reducer: {
+    // The cart reducer is added to the store
     cart: cartReducer,
   },
 });
